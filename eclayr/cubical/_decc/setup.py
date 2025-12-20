606be1c0271db2dead48da28e1a0b953e0e82e1c
bossacode <hlee2745@gmail.com>
2025-12-21 00:48:41 +0900

    Cython code for DECC

---
+++ eclayr/cubical/_decc/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, Extension
+from Cython.Build import cythonize
+from Cython.Compiler import Options
+import numpy as np
+# import sys
+
+# if sys.platform.startswith("win"):
+#     openmp_arg = '/openmp'
+# else:
+#     openmp_arg = '-fopenmp'
+
+Options.annotate = True
+
+extensions = [
+    # Extension(
+    #     name="decc",
+    #     sources=["decc.pyx"],
+    #     extra_compile_args=[openmp_arg],
+    #     extra_link_args=[openmp_arg]
+    #     ),
+    Extension(
+        name="decc",
+        sources=["decc.pyx"],
+        include_dirs=[np.get_include()]
+        )
+]
+
+setup(
+    name="decc",
+    ext_modules=cythonize(extensions, annotate=True)
+)
