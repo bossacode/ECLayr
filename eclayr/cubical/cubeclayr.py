790a50f783889521e78d3c41f58a346e82c80bf7
bossacode <hlee2745@gmail.com>
2025-12-26 23:47:07 +0900

    Add "auto" option for beta

--- eclayr/cubical/cubeclayr.py
+++ eclayr/cubical/cubeclayr.py
@@ -12,7 +12,7 @@ class ECCwrapper(Function):
     def forward(ctx, x, func, device):
         """
         Args:
-            x (tensor of shape (B, C, H, W)): Batch of 2D input images.
+            x (tensor of shape (B, C, H, W) or (B, C, D, H, W)): Batch of 2D or 3D input images.
             func (function or method): Function that computes the Euler characteristic curves (and gradients if necessary).
             device (str or torch.device): Device to which the output (and gradient) will be moved.
 
@@ -38,9 +38,9 @@ class ECCwrapper(Function):
         """
         grad_in = None
         if ctx.needs_input_grad[0]:
-            grad_local, = ctx.saved_tensors                                     # shape: (B, C, H*W, steps)
-            grad_in = torch.einsum("...ij,...j->...i", grad_local, grad_out)    # shape: (B, C, H*W)
-            grad_in = grad_in.view(*ctx.input_shape)                            # shape: (B, C, H, W)
+            grad_local, = ctx.saved_tensors                                     # shape: (B, C, H*W, steps) or (B, C, D*H*W, steps)
+            grad_in = torch.einsum("...ij,...j->...i", grad_local, grad_out)    # shape: (B, C, H*W) or (B, C, D*H*W)
+            grad_in = grad_in.view(*ctx.input_shape)                            # shape: (B, C, H, W) or (B, C, D, H, W)
         return grad_in, None, None
 
 
@@ -52,20 +52,20 @@ class CubECLayr(nn.Module):
             interval (Iterable[float], optional): Interval of filtration values to be considered. Defaults to [0., 1.].
             steps (int, optional):  Number of discretized points. Defaults to 32.
             sublevel (bool, optional): Whether to use sublevel set filtration. If False, superlevel set filtration will be used. Defaults to True.
-            beta (float, optional): Controls the magnitude of impulse that approximates the dirac delta function used for backpropagation. Smaller values yield higher impulse. Defaults to 0.1.
+            beta (float or "auto", optional): Controls the magnitude of impulse that approximates the dirac delta function used for backpropagation. Defaults to 0.1.
             postprocess (nn.Module, optional): Postprocessing layer. Defaults to nn.Identity().
             device (str or torch.device): Device to which the output (and gradient) will be moved. Defaults to "cpu".
         """
         assert len(interval) == 2, "Interval must consist of two values."
         assert interval[1] > interval[0], "End point of the interval must be larger than the starting point."
         assert steps > 1, "Steps must be larger than 1."
+        assert beta == "auto" or isinstance(beta, (float, int)), "Beta must be either a number or 'auto'."
 
         super().__init__()
         self.interval = interval if sublevel else [-i for i in reversed(interval)] # change interval when superlevel set filtration is used
         self.steps = steps
         self.sublevel = sublevel
-        self.impulse = 1 / (abs(beta) * math.sqrt(math.pi))
-        # self.impulse = (steps-1) / (interval[1] - interval[0])  # equivalent to setting beta = 2delta_t / sqrt(pi).
+        self.impulse = (steps-1) / (interval[1]-interval[0]) if beta == "auto" else 1 / (abs(beta)*math.sqrt(math.pi))  # equivalent to beta = 2delta_t / sqrt(pi) when "auto"
         self.postprocess = postprocess
         self.device = device
         self.func = None
