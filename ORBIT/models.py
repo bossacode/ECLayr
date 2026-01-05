81641217e2c307d91b0196b24b9067da6a8c0f40
bossacode <hlee2745@gmail.com>
2026-01-06 02:03:46 +0900

    Input image size is now (28, 28) instead of (40, 40), so change Cnn kernel size accordingly. Use DTM for second topo layer in ECCnn and DECCnn.

--- ORBIT/models.py
+++ ORBIT/models.py
@@ -4,6 +4,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from eclayr.cubical.cubeclayr import CubECLayr, CubDECC
+from utils.dtm import WeightedDTMLayer
 
 
 # Cnn
@@ -11,12 +12,12 @@ class Cnn(nn.Module):
     def __init__(self, in_channels=1, num_classes=5, *args, **kwargs):
         super().__init__()
         self.conv = nn.Sequential(
-            nn.Conv2d(in_channels=in_channels, out_channels=32, kernel_size=4, stride=1, padding="same"),
+            nn.Conv2d(in_channels=in_channels, out_channels=32, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
-            nn.Conv2d(in_channels=32, out_channels=1, kernel_size=4, stride=1, padding="same")
+            nn.Conv2d(in_channels=32, out_channels=1, kernel_size=3, stride=1, padding=1)
             )
         self.fc = nn.Sequential(
-            nn.Linear(1600, 64),
+            nn.Linear(784, 64),
             nn.ReLU(),
             nn.Linear(64, num_classes)
             )
@@ -34,7 +35,7 @@ class ECCnn_i(Cnn):
         super().__init__(in_channels, num_classes)
         self.eclayr = CubECLayr(postprocess=nn.Linear(kwargs["steps"], kwargs["topo_out"]), *args, **kwargs)
         self.fc = nn.Sequential(
-            nn.Linear(1600 + kwargs["topo_out"], 64),
+            nn.Linear(784 + kwargs["topo_out"], 64),
             nn.ReLU(),
             nn.Linear(64, num_classes)
             )
@@ -52,29 +53,50 @@ class ECCnn_i(Cnn):
 class ECCnn(Cnn):
     def __init__(self, in_channels=1, num_classes=5, *args, **kwargs):
         super().__init__(in_channels, num_classes)
-        self.eclayr_1 = CubECLayr(interval=kwargs["interval_1"], sublevel=kwargs["sublevel_1"],
+        self.eclayr1 = CubECLayr(interval=kwargs["interval_1"], sublevel=kwargs["sublevel_1"],
                                   postprocess=nn.Linear(kwargs["steps"], kwargs["topo_out"]), *args, **kwargs)
-        self.eclayr_2 = CubECLayr(interval=kwargs["interval_2"], sublevel=kwargs["sublevel_2"],
+        self.eclayr2 = CubECLayr(interval=kwargs["interval_2"], sublevel=kwargs["sublevel_2"],
                                   postprocess=nn.Linear(kwargs["steps"], kwargs["topo_out"]), *args, **kwargs)
         self.fc = nn.Sequential(
-            nn.Linear(1600 + 2*kwargs["topo_out"], 64),
+            nn.Linear(784 + 2*kwargs["topo_out"], 64),
             nn.ReLU(),
             nn.Linear(64, num_classes)
             )
+        self.dtm = WeightedDTMLayer(m0=0.02, size=[28, 28])
 
     def forward(self, x):
         x, x_dtm = x
-        ecc_1 = F.relu(self.eclayr_1(x_dtm))    # first ECLayr
-        x = F.relu(self.conv(x))                # CNN
+        ecc1 = F.relu(self.eclayr1(x_dtm))  # first ECLayr
+        ##########################################
+        # x = F.relu(self.conv(x))                # CNN
+        x = self.conv(x)                # CNN
+        ##########################################
 
-        # second ECLayr after conv layer
-        max_vals = x.amax(dim=(2, 3), keepdim=True)     # shape: (B, C, 1, 1)
-        if (max_vals != 0).all():
-            ecc_2 = F.relu(self.eclayr_2(x / max_vals)) # normalize between 0 and 1 for each data and channel
-        else:
-            ecc_2 = F.relu(self.eclayr_2(x))
+
+        # # after ReLU
+        # x = F.relu(x)
+        # x_max = x.detach().amax(dim=(2, 3), keepdim=True)   # shape: (B, C, 1, 1)
+        # if (x_max != 0).all():
+        #     ecc_2 = F.relu(self.eclayr_2(x / x_max)) # normalize between 0 and 1 for each data and channel
+        # else:
+        #     ecc_2 = F.relu(self.eclayr_2(x))
+
+
+        # # before ReLU
+        # x_max = x.detach().amax(dim=(2, 3), keepdim=True)   # shape: (B, C, 1, 1)
+        # x_min = x.detach().amin(dim=(2, 3), keepdim=True)   # shape: (B, C, 1, 1)
+        # ecc_2 = F.relu(self.eclayr_2((x-x_min) / (x_max-x_min)))    # min-max normalization between 0 and 1 for each data and channel
+
+
+        # before ReLU with DTM
+        x_max = x.detach().amax(dim=(2, 3), keepdim=True)   # shape: (B, C, 1, 1)
+        x_min = x.detach().amin(dim=(2, 3), keepdim=True)   # shape: (B, C, 1, 1)
+        x_dtm2 = self.dtm((x-x_min) / (x_max-x_min))
+        x_dtm2_max = x_dtm2.detach().amax(dim=(2, 3), keepdim=True)
+        x_dtm2_min = x_dtm2.detach().amin(dim=(2, 3), keepdim=True)
+        ecc2 = F.relu(self.eclayr2((x_dtm2-x_dtm2_min) / (x_dtm2_max-x_dtm2_min)))
         
-        x = torch.concat((x.flatten(1), ecc_1, ecc_2), dim=-1)
+        x = torch.concat((F.relu(x).flatten(1), ecc1, ecc2), dim=-1)
         x = self.fc(x)
         return x
 
@@ -83,29 +105,31 @@ class ECCnn(Cnn):
 class DECCnn(Cnn):
     def __init__(self, in_channels=1, num_classes=5, *args, **kwargs):
         super().__init__(in_channels, num_classes)
-        # self.decc_1 = CubDECC(interval=kwargs["interval_1"], sublevel=kwargs["sublevel_1"], lam=kwargs["lam_1"], postprocess=nn.Linear(kwargs["steps_1"], topo_out_units), *args, **kwargs)
-        self.eclayr_1 = CubECLayr(interval=kwargs["interval_1"], sublevel=kwargs["sublevel_1"],
+        # self.decc1 = CubDECC(interval=kwargs["interval_1"], sublevel=kwargs["sublevel_1"], lam=1000, postprocess=nn.Linear(kwargs["steps_1"], topo_out_units), *args, **kwargs)
+        self.eclayr1 = CubECLayr(interval=kwargs["interval_1"], sublevel=kwargs["sublevel_1"],
                                   postprocess=nn.Linear(kwargs["steps"], kwargs["topo_out"]), *args, **kwargs)
-        self.decc_2 = CubDECC(interval=kwargs["interval_2"], sublevel=kwargs["sublevel_2"],
+        self.decc2 = CubDECC(interval=kwargs["interval_2"], sublevel=kwargs["sublevel_2"],
                                   postprocess=nn.Linear(kwargs["steps"], kwargs["topo_out"]), *args, **kwargs)
         self.fc = nn.Sequential(
-            nn.Linear(1600 + 2*kwargs["topo_out"], 64),
+            nn.Linear(784 + 2*kwargs["topo_out"], 64),
             nn.ReLU(),
             nn.Linear(64, num_classes)
             )
+        self.dtm = WeightedDTMLayer(m0=0.02, size=[28, 28])
 
     def forward(self, x):
         x, x_dtm = x
-        ecc_1 = F.relu(self.eclayr_1(x_dtm))    # first DECC (replaced with EClayr for computational reasons as the this layer does not require backpropagation)
-        x = F.relu(self.conv(x))                # CNN
+        ecc1 = F.relu(self.eclayr1(x_dtm))  # first DECC (replaced with EClayr for computational reasons as the this layer does not require backpropagation)
+        x = self.conv(x)                    # CNN
 
-        # second DECC after conv layer
-        max_vals = x.amax(dim=(2, 3), keepdim=True)     # shape: (B, C, 1, 1)
-        if (max_vals != 0).all():
-            ecc_2 = F.relu(self.decc_2(x / max_vals))   # normalize between 0 and 1 for each data and channel
-        else:
-            ecc_2 = F.relu(self.decc_2(x))
+        # before ReLU with DTM
+        x_max = x.detach().amax(dim=(2, 3), keepdim=True)   # shape: (B, C, 1, 1)
+        x_min = x.detach().amin(dim=(2, 3), keepdim=True)   # shape: (B, C, 1, 1)
+        x_dtm2 = self.dtm((x-x_min) / (x_max-x_min))        # normalize between 0 and 1 for each data and channel
+        x_dtm2_max = x_dtm2.detach().amax(dim=(2, 3), keepdim=True)
+        x_dtm2_min = x_dtm2.detach().amin(dim=(2, 3), keepdim=True)
+        ecc2 = F.relu(self.decc2((x_dtm2-x_dtm2_min) / (x_dtm2_max-x_dtm2_min)))
         
-        x = torch.concat((x.flatten(1), ecc_1, ecc_2), dim=-1)
+        x = torch.concat((F.relu(x).flatten(1), ecc1, ecc2), dim=-1)
         x = self.fc(x)
         return x
