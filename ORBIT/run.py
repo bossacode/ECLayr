791128c12f6f1f147ff676ccaab54171788ed9a2
bossacode <hlee2745@gmail.com>
2026-02-04 02:56:33 +0900

    Run 15 simulations excluding training failure

--- ORBIT/run.py
+++ ORBIT/run.py
@@ -1,9 +1,11 @@
 import sys
 sys.path.append("../")
 import torch
-import wandb
+import pandas as pd
+# import wandb
 import argparse
 import yaml
+import os
 from utils.train_eval import run, run_wandb
 from models import Cnn, ECCnn_i, ECCnn, DECCnn
 
@@ -13,55 +15,64 @@ torch.backends.cudnn.benchmark = False
 torch.backends.cudnn.deterministic = True
 torch.manual_seed(42)
 
-# argument parser
-parser = argparse.ArgumentParser()
-parser.add_argument("-m", "--model_flag", help="Name of model to train")
-args = parser.parse_args()
-
-# load configuration file needed for training model
-with open(f"configs/{args.model_flag}.yaml", "r") as f:
-    cfg = yaml.load(f, yaml.FullLoader)
-
-# add device to configuration file
-if torch.cuda.is_available():
-    cfg["device"] = "cuda"
-elif torch.backends.mps.is_available():
-    cfg["device"] = "mps"
-else:
-    cfg["device"] = "cpu" 
-
-model_dict = {
-    "Cnn": Cnn,
-    "ECCnn_i": ECCnn_i,
-    "ECCnn": ECCnn,
-    "DECCnn": DECCnn
-    }
-
 
 if __name__ == "__main__":
+    # argument parser
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-m", "--model_flag", help="Name of model to train")
+    args = parser.parse_args()
+
+    # load configuration file needed for training model
+    with open(f"configs/{args.model_flag}.yaml", "r") as f:
+        cfg = yaml.load(f, yaml.FullLoader)
+
+    # add device to configuration file
+    if torch.cuda.is_available():
+        cfg["device"] = "cuda"
+    elif torch.backends.mps.is_available():
+        cfg["device"] = "mps"
+    else:
+        cfg["device"] = "cpu" 
+
+    model_dict = {
+        "Cnn": Cnn,
+        "ECCnn_i": ECCnn_i,
+        "ECCnn": ECCnn,
+        "DECCnn": DECCnn
+        }
+
     nsim = 15                                   # number of simulations to run
     noise_prob = [0.0, 0.05, 0.1, 0.15, 0.2]    # noise probabilities
+    
+    result_dir = "./results"    # directory to save results
+    os.makedirs(result_dir, exist_ok=True)
 
-    wandb.login()
-    project = "ORBIT"           # used as project name in wandb
-    group = args.model_flag     # used for grouping experiments in wandb
-
+    # wandb.login()
+    
+    print(f"Training {args.model_flag} model...")
+    print(cfg)
+    history = []
     for p in noise_prob:
         prob = str(int(p * 100)).zfill(2)
-        job_type = prob                     # used for grouping experiments in wandb
         data_dir = f"./dataset/{prob}/"     # base directory path to where data is loaded
         
-        print("-"*30)
-        print(f"Corruption & noise rate: {p}")
-        print("-"*30)
-        
         # loop over number of simulations
-        for sim in range(1, nsim+1):
-            print(f"\nSimulation: [{sim} / {nsim}]")
-            print("-"*30)
+        sim = 1
+        while sim <= nsim:
+            print("\n")
+            print("-"*60)
+            print(f"Corruption & noise rate: {p}")
+            print(f"Simulation: [{sim} / {nsim}]")
+            print("-"*60)
             
-            name = f"sim{sim}"  # used for specifying runs in wandb
             model = model_dict[args.model_flag](device=cfg["device"], **cfg["model_params"]).to(cfg["device"])
-            
-            run_wandb(model, cfg, data_dir, project, group, job_type, name)
-            # run(model, cfg, data_dir)
+            test_acc, train_acc, runtime, best_epoch = run(model, cfg, data_dir)
+            # run_wandb(model, cfg, data_dir, project="ORBIT", group=args.model_flag, job_type=prob, name=f"sim{sim}")
+            if train_acc < 80:
+                print(f"Train accuracy {train_acc} is lower than 80%, repeating simulation {sim}...")
+                continue
+            history.append((int(p * 100), train_acc, test_acc, runtime, best_epoch))
+            sim += 1
+
+    df = pd.DataFrame(history, columns=["Job Type", "Train Accuracy", "Test Accuracy", "Runtime", "Best Epoch"])
+    df.to_csv(f"{result_dir}/{args.model_flag}.csv", index=False)
